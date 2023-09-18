19753c564d75b438e829326ae0435f87e5a75050
alish <aalishevaa@yandex.ru>
2023-09-18 15:02:33 +0600

    Дипломка main_svc 2-этап

--- server-ewm/src/main/java/ru/practicum/server/controller/AdminController.java
+++ server-ewm/src/main/java/ru/practicum/server/controller/AdminController.java
@@ -11,7 +11,6 @@ import ru.practicum.server.service.CompilationService;
 import ru.practicum.server.service.EventService;
 import ru.practicum.server.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
